ea2c7af4629fbe07aed7c0f9be4bb73bb77c389f
CalebChoo1 <S10205375@connect.np.edu.sg>
2021-01-01 22:48:17 +0800

    movie section javascript done

--- ID_S10205375_Choo Jun Le Caleb_Assg2_website/script.js
+++ ID_S10205375_Choo Jun Le Caleb_Assg2_website/script.js
@@ -6,7 +6,6 @@ $.ajax({
     "Authorization": "Bearer vfSSlo3EYHKB5s0VEep4"
   },
   success: function (result){
-      console.log(result);
       var book1 = result.docs[0].name;
       var book2 = result.docs[1].name;
       var book3 = result.docs[2].name;
@@ -15,3 +14,35 @@ $.ajax({
       $("#book3").html(book3);
   }
 });
+
+var button = document.querySelector(".btn");
+var movName = document.querySelector("#movname");
+var runtime = document.querySelector("#runtime");
+var budget = document.querySelector("#budget");
+var revenue = document.querySelector("#revenue");
+var nominations = document.querySelector("#nominations");
+var wins = document.querySelector("#wins")
+var rotten = document.querySelector("#rotten");
+
+button.addEventListener("click", function(){
+  fetch("https://the-one-api.dev/v2/movie", {
+    headers: {
+      "Authorization": "Bearer vfSSlo3EYHKB5s0VEep4"
+    }
+  })
+  .then(response => response.json()) 
+  .then(function(data){
+    var movieName = document.getElementById("searchTxt").value;
+    for (let i = 0; i < data.docs.length; i++){
+      if (movieName === data.docs[i].name){
+        movName.innerText = data.docs[i].name;
+        runtime.innerText = data.docs[i].runtimeInMinutes;
+        budget.innerText = data.docs[i].budgetInMillions;
+        revenue.innerText = data.docs[i].boxOfficeRevenueInMillions;
+        nominations.innerText = data.docs[i].academyAwardNominations;
+        wins.innerText = data.docs[i].academyAwardWins;
+        rotten.innerText = data.docs[i].rottenTomatesScore;
+      }
+    }
+  });
+})
